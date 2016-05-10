324412be9c86fa42048b3f31bc648ebd864eb0a3
Mike Griffith <bikegriffith@gmail.com>
2016-05-09 21:29:17 -0400

    Update example-slack-config.js

--- example-slack-config.js
+++ example-slack-config.js
@@ -1,6 +1,6 @@
 // example file if you want slack integration for deployment
 module.exports = {
-  url: '<my slack service webhook url>'
+  url: '<my slack service webhook url>',
   channel: '#my-channel',
   user: 'gulp-bot',
   icon_emoji: ':triumph:'
